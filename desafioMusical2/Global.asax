ae55302ce96daae75555a37268c7c3abc4ade6f9
pedro <pedro@DESKTOP-M8Q85VP>
2016-09-25 15:39:34 -0300

    Add project files.

---
+++ desafioMusical2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="desafioMusical2.MvcApplication" Language="C#" %>
---
+++ desafioMusical2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(desafioMusical2.Startup))]
+namespace desafioMusical2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
