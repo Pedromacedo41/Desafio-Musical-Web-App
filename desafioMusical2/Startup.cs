5f058641d0880906acc4ad585544efb9cec89f0e
pedro <pedro@DESKTOP-M8Q85VP>
2016-09-25 15:53:49 -0300

    Deleting pre-definied login resources

--- desafioMusical2/Startup.cs
+++ desafioMusical2/Startup.cs
@@ -6,9 +6,6 @@ namespace desafioMusical2
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
+        
     }
 }
